a2fd6778dbe519d719fd8d29057b29b27a178c09
frytzios <francomacbook8@gmail.com>
2026-01-20 14:57:33 -0300

    user routers y controllers agregados

--- backend/src/server.js
+++ backend/src/server.js
@@ -7,6 +7,7 @@ import { clerkMiddleware } from '@clerk/express';
 import {serve} from 'inngest/express'
 import {functions , inngest} from './config/inngest.js';
 import adminRoutes  from './routes/admin.route.js';
+import userRoutes  from './routes/user.route.js';
 
 
 
@@ -18,6 +19,7 @@ app.use(clerkMiddleware()) ;
 app.use(express.json());
 app.use('/api/inngest', serve({client:inngest, functions}));
 app.use('/api/admin' , adminRoutes)
+app.use('/api/users' , userRoutes)
 
 app.get("/api/health", (req, res) => {
     res.status(200).json({ message: "Succes" });
